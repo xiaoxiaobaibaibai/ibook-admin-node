e3c352af8f8b9b5a4b69ca96fac02358aed53088
bai <2516220191@qq.com>
2020-07-21 23:37:09 +0800

    前后台接口初步对接

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = require('./router')
-const app = express()
-
+const fs = require('fs')
+const https = require('https')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 // function myLogger(req, res, next) {
 //     console.log('myLogger')
 //     next()
@@ -24,9 +26,27 @@ const app = express()
 //
 // app.use(errorHandler)
 
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
 app.use('/', router)
 
+const privateKey = fs.readFileSync('./https/4235134_fdxans.com.key')
+const pem = fs.readFileSync('./https/4235134_fdxans.com.pem')
+
+const credentials = {
+    key: privateKey,
+    cert: pem
+}
+
+const  httpsServer = https.createServer(credentials, app)
 const server = app.listen(5000, function () {
     const { address, port } = server.address()
     console.log('Http Server is running on http://%s%s', address, port)
+})
+
+httpsServer.listen(18082, function () {
+    console.log('Https Server is running on http://localhost:%s', 18082)
 })
--- router/user.js
+++ router/user.js
@@ -2,8 +2,25 @@ const express = require('express')
 
 const router = express.Router()
 
+router.post('login', function (req, res) {
+    console.log(req.body)
+
+    res.json({
+        code: 0,
+        msg: 'login success'
+    })
+})
+
 router.get('/info', function(req, res, next) {
     res.json('user info...')
+})
+
+router.post('/login', function(req, res, next) {
+    console.log('/user/login', req.body)
+    res.json({
+        code: 0,
+        msg: '登录成功'
+    })
 })
 
 module.exports = router
